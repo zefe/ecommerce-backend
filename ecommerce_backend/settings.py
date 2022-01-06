ebc89cd5476ab253af6079a0634b35def6adc6b8
zefe torres <zefetorres@icloud.com>
2022-01-06 09:43:20 -0600

    change static files

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -88,7 +88,6 @@ SIMPLE_JWT = {
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -178,7 +177,6 @@ STATICFIELDS_DIRS = [
 ]
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
